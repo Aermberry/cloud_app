59f1347dcac013ae744603a0ab2ddda024a3eb01
刘成恩 <ENEN@DESKTOP-OMA4GTP>
2018-02-24 09:45:57 +0800

    更新applay

--- src/service/api/debt.api.js
+++ src/service/api/debt.api.js
@@ -2,18 +2,9 @@ import http from 'src/service/common/http'
 
 export default {
   //  债事申请
-  apply () {
-    var response = http.post('User/Debt/apply')
-    return response
-  },
-  // 商品详情
-  show (data) {
-    var response = http.get('/product/show', data)
-    return response
-  },
-  // 商品列表
-  list (data) {
-    var response = http.get('/product/list', data)
+  async apply (data) {
+    console.dir(data)
+    var response = await http.post('User/Debt/apply', data)
     return response
   }
 }
--- src/views/open/debt/apply.vue
+++ src/views/open/debt/apply.vue
@@ -4,19 +4,17 @@
     <zk-head title='债事报备' goBack='首页'></zk-head>
     <div>
       <group gap="0.51rem 0rem" id="form">
-        <x-input title="债务人姓名" required :min="2" :max="12" v-model="apply.name"></x-input>
-        <x-input title="债务人手机号" required placeholder="请输入债务人的手机号" mask="999 9999 9999" v-model="apply.mobile" :max="13" is-type="mobile"></x-input>
+        <x-input title="债务人姓名" required :min="2" :max="12" v-model="debtApiInput.name"></x-input>
+        <x-input title="债务人手机号" required placeholder="请输入债务人的手机号" mask="999 9999 9999" v-model="debtApiInput.mobile" :max="13" is-type="mobile"></x-input>
         <x-input title="债务人身份证号" required :max="18" is-type="idcard"></x-input>
       </group>
       <group>
-        <group-title slot="title">I'm a title
-          <span style="float:right;">right</span>
-        </group-title>
+        <group-title slot="title">债市详情</group-title>
         <box gap="0.2rem 0.2rem">
           <x-input title="债事金额" required type="number" is-type="currency" class="zk-border-bottom"></x-input>
           <x-textarea title="债事过程" class="zk-border-bottom"></x-textarea>
           <x-upload :fileCount="20" :savePath="savePath" :size="5*1024" ref="savePath">相关资料</x-upload>
-          <x-button @click.native="apiPost" type="primary" action-type="button">下一步</x-button>
+          <x-button @click.native="apiPost" type="primary" action-type="button">申请</x-button>
         </box>
       </group>
     </div>
@@ -25,7 +23,7 @@
 </template>
 
 <script>
-  // import  apiService from 'src/service/api/debt.api'
+  import apiService from 'src/service/api/debt.api'
   import { Group, XInput, Box, XTextarea, XUpload, GroupTitle, XButton } from 'zkui'
   export default {
     components: {
@@ -36,13 +34,13 @@
         savePath: '/open/debt',
         files: [],
         step2: 0,
-        apply: {
-          name: '',
-          mobile: '',
+        debtApiInput: {
+          name: 'ddd',
+          mobile: '13865466956',
           IDCard: '',
           DebtProcess: '',
           Attachment: '',
-          Amount: ''
+          Amount: '12.03'
         }
       }
     },
@@ -51,8 +49,13 @@
         console.dir(this.$ref)
       },
       async apiPost () {
-        //  var repsonse = await apiService.apply(this.apply)
-        //  console.dir(repsonse)
+        var response = await apiService.apply(this.apply)
+        console.dir(response)
+        if (response.data.status === 1) {
+          this.messageSuccess('申请成功')
+        } else {
+          this.$vux.toast.warn(response.data.message)
+        }
       }
     }
   }
