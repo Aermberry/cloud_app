8b48814daf6eb53a504abf3b0ce07fcbda6d52d7
sampsong <sampsong@qq.com>
2018-03-02 07:35:12 +0800

    更新订单

---
+++ src/service/api/order.api.js
@@ -0,0 +1,25 @@
+import http from 'src/service/common/http'
+
+export default {
+  //  根据商品信息，确认订单页面，获取商品的购买信息,包括价格，物理、等计算
+  buyProduct (data) {
+    var response = http.getLogin('/order/GetBuyInfo', data)
+    return response
+  },
+  //  提交订单，购物商品
+  Buy (data) {
+    var response = http.postLogin('/order/buy', data)
+    return response
+  },
+  //  我的订单
+  list (data) {
+    var response = http.getLogin('/order/list', data)
+    return response
+  },
+  //  订单详情页
+  show (data) {
+    var response = http.getLogin('/order/show', data)
+    return response
+  }
+
+}
