8da59b80f360bcf00976b1b058fee22ef1eb0635
刘成恩 <2630039223@qq.com>
2018-03-03 14:44:02 +0800

    user.api.js

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,51 +1,36 @@
 <template>
+  <section class="zkui-reward-list">
 
-  <div class="zk-class-tab">
-
-  </div>
-
+    <zk-head title='财务记录' goBack='我的钱包'></zk-head>
+    <zk-foot></zk-foot>
+  </section>
 </template>
 
-
-
 <script>
-  import { GroupTitle, Swipeout, SwipeoutItem, SwipeoutButton, XButton } from 'zkui'
+  import apiService from 'src/service/api/user.api'
   export default {
-    components: {
-    },
     data () {
       return {
-        GroupTitle,
-        Swipeout,
-        SwipeoutItem,
-        SwipeoutButton,
-        XButton
+        viewModel: ''
       }
     },
+    mounted () {
+      this.GetData()
+      this.Tree()
+    },
     methods: {
+      async GetData () {
+        var reponse = await apiService.QrCode(this.data)
+        console.log(reponse.data)
+        console.dir(reponse.data)
+      },
+      async Tree () {
+        var reponse = await apiService.Tree(this.data)
+        console.dir(reponse.data)
+      }
     }
   }
 </script>
 
-<style>
-  .zk-class-tab {
-    width: 100%;
-    display: -moz-box;
-    display: -ms-flexbox;
-    display: -webkit-box;
-    display: -webkit-flex;
-    display: flex;
-    height: auto;
-  }
-  .zk-class-tab-left {
-    width: 75px;
-    display: block !important;
-  }
-  .zk-class-tab-right {
-    -ms-flex: 1;
-    -moz-box-flex: 1;
-    -webkit-flex: 1;
-    flex: 1;
-  }
-</style>
+
 
