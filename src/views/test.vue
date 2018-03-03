f167979ad6359424887b2c9272de0a6edff0af4d
sampsong <sampsong@qq.com>
2018-03-03 19:52:34 +0800

    解决冲突

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,22 +1,17 @@
 <template>
   <section class="zkui-reward-list">
 
-    <zk-head title='财务记录' goBack='我的钱包'></zk-head>
+    <zk-head title='' goBack=''></zk-head>
     <zk-foot></zk-foot>
   </section>
 </template>
 
 <script>
-  import apiService from 'src/service/api/user.api'
+
   export default {
-<<<<<<< .mine
-    data () {
-      return {
-=======
     components: {
 
->>>>>>> .theirs
-        viewModel: ''
+      viewModel: ''
     },
     data () {
       return {
@@ -28,38 +23,18 @@
       this.Tree()
     },
     methods: {
-<<<<<<< .mine
-      async GetData () {
-        var reponse = await apiService.QrCode(this.data)
-        console.log(reponse.data)
-        console.dir(reponse.data)
-      },
-      async Tree () {
-        var reponse = await apiService.Tree(this.data)
-        console.dir(reponse.data)
-=======
       onButtonClick (type) {
         alert('on button click ' + type)
       },
       handleEvents (type) {
         console.log('event: ', type)
-
-
-
->>>>>>> .theirs
       }
     }
   }
 </script>
 
-<<<<<<< .mine
-
-
-
-=======
 <style>
 
 </style>
->>>>>>> .theirs
 
 
