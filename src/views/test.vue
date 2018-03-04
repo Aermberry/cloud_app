858560edd2055a97b2aec005da84350af7118d8c
hui <2372849697@qq.com>
2018-03-04 10:03:57 +0800

    1

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,65 +1,17 @@
 <template>
   <section class="zkui-reward-list">
 
-    <zk-head title='²ÆÎñ¼ÇÂ¼' goBack='ÎÒµÄÇ®°ü'></zk-head>
+    <zk-head title='ï¿½ï¿½ï¿½ï¿½ï¿½Â¼' goBack='ï¿½Òµï¿½Ç®ï¿½ï¿½'></zk-head>
     <zk-foot></zk-foot>
   </section>
 </template>
 
 <script>
-  import apiService from 'src/service/api/user.api'
-  export default {
-<<<<<<< .mine
-    data () {
-      return {
-=======
-    components: {
 
->>>>>>> .theirs
-        viewModel: ''
-    },
-    data () {
-      return {
-        disabled: false
-      }
-    },
-    mounted () {
-      this.GetData()
-      this.Tree()
-    },
-    methods: {
-<<<<<<< .mine
-      async GetData () {
-        var reponse = await apiService.QrCode(this.data)
-        console.log(reponse.data)
-        console.dir(reponse.data)
-      },
-      async Tree () {
-        var reponse = await apiService.Tree(this.data)
-        console.dir(reponse.data)
-=======
-      onButtonClick (type) {
-        alert('on button click ' + type)
-      },
-      handleEvents (type) {
-        console.log('event: ', type)
-
-
-
->>>>>>> .theirs
-      }
-    }
-  }
 </script>
 
-<<<<<<< .mine
 
 
 
-=======
-<style>
-
-</style>
->>>>>>> .theirs
 
 
