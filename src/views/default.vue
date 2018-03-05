3fe8ebc059829dab440d7e982d80539b5d762978
sampsong <sampsong@qq.com>
2018-03-06 05:53:02 +0800

    解决首页图标不显示问题

--- src/views/default.vue
+++ src/views/default.vue
@@ -7,7 +7,7 @@
       <m-icon name="zk-qrcode1" class="icon-brand index-qrcode mr-1 mt-1" link="/user/qrcode" size="1rem"></m-icon>
     </div>
     <zk-swiper diykey="swiper_index" height="210px"></zk-swiper>
-    <zk-grid diykey="grid_index" :cols="4" class="zkui-grid-border__none border-bottom "></zk-grid>
+    <zk-grid diykey="grid_index" :cols="4" class="zkui-grid-border__none border-bottom grid-icon-middle "></zk-grid>
 
     <group-title>流行单品</group-title>
     <zk-product-item :pageSize=4 classIds='' tagsId='' diykey='index'></zk-product-item>
