fda4ef2504b12111ee6166e02c80130d0045bdab
sampsong <sampsong@qq.com>
2018-03-20 14:15:24 +0800

    更新

--- src/service/common/helper.js
+++ src/service/common/helper.js
@@ -1,34 +1,22 @@
 // 常用帮助函数
 'use strict'
 import store from 'src/store/index'
+import weui from 'weui.js'
 export default {
   // 操作成功提示函数
   alertSucess (data) {
-    // 使用mint-ui的toast函数  http://mint-ui.github.io/docs/#/en2/toast
-    console.info(data)
-   // alert(data)
-    // this.$vux.toast.success('data')
-    // weui.toast(data)
+    weui.toast(data, 3000)
   },
   // 操作失败提示函数
   alertError (data) {
-    console.error(data)
-   // alert(data)
-    //  weui.toast(data)
+    weui.toast(data, 3000)
   },
-
-  // 在开发模式下输出类的详细
-  console (data) {
-    if (process.env.NODE_ENV === 'development') {
-      console.dir(data)
-    }
-  },
-
   // 判断用户是否登录，如果没有登陆则跳转到登录页面，或者不执行操作
   checkLogin (isPush = false) {
     var loginUser = store.state.userStore.loginUser
     if (loginUser === null) {
       if (isPush === true) {
+        weui.toast('请先登录', 3000)
         location.href = '/user/login'
       }
       return false
