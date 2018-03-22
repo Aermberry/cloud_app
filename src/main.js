210f1d33a4ec8b099e6a351b8c6bceb2cf37a56e
hui <2372849697@qq.com>
2018-03-22 17:59:27 +0800

    PC首页

--- src/main.js
+++ src/main.js
@@ -3,12 +3,24 @@ import App from './App'
 import router from 'src/service/router/router'
 import store from 'src/store/index'
 
-import { MIcon, zkui, bootstrap, ToastPlugin } from 'zkui'
-import { ZkHead, ZkFoot } from 'src/widgets'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import {
+  MIcon,
+  zkui,
+  bootstrap,
+  ToastPlugin
+} from 'zkui'
+import {
+  ZkHead,
+  ZkFoot
+} from 'src/widgets'
 import 'src/service/config/rem'
 import FastClick from 'fastclick'
 import global from 'src/service/common/global' // 公共函数,全局函数
 
+Vue.use(ElementUI)
 Vue.use(ToastPlugin)
 Vue.use(global)
 
