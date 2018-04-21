349eb3c8550ef0a1ef74e105d92e616f0a818e84
hui <2372849697@qq.com>
2018-04-21 17:51:04 +0800

    修改

--- src/views/test2.vue
+++ src/views/test2.vue
@@ -1,11 +1,5 @@
 <template>
   <div>
-    <!-- <vue-preview :slides="slide1" @close="handleClose"></vue-preview> -->
-    <a href="http://wpa.qq.com/msgrd?V=1&amp;uin=2630039223&amp;Site=%E7%99%BE%E6%96%AF%E7%89%B9%E7%85%A7%E6%98%8E&amp;Menu=yes" target="_blank">
-      <i><img src="/static/images/dasheng/find6.png"></i>
-      消息</a>
-
-    <a target="_blank" href="mqqwpa://im/chat?chat_type=wpa&uin=1139936790&version=1&src_type=web&web_src=lvlingseeds.com">手机QQ客服</a>
   </div>
 </template>
 <script>
@@ -13,11 +7,11 @@
     components: {
     },
     mounted () {
-      // // 云债通八图暂用这页面提示
-      // this.$vux.toast.show('敬请期待')
-      // setTimeout(function () {
-      //   this.$router ? this.$router.back() : window.history.back()
-      // }, 2000)
+      // 云债通八图暂用这页面提示
+      this.$vux.toast.show('敬请期待')
+      setTimeout(function () {
+        this.$router ? this.$router.back() : window.history.back()
+      }, 2000)
     },
     data () {
       return {
