bf9dfe857b94dc3640363c287603359fafa67453
hui <2372849697@qq.com>
2018-06-08 16:17:46 +0800

    4

--- src/service/config/env.js
+++ src/service/config/env.js
@@ -22,10 +22,10 @@ if (process.env.NODE_ENV === 'development') {
   // baseUrl = 'http://admin.czhait.com'
   // baseUrl = 'http://zkgs.5ug.com'
   // baseUrl = 'http://zkqd.5ug.com'
-   baseUrl = 'http://zqingchun.yiqipingou.com/'
+  baseUrl = 'http://zqingchun.yiqipingou.com/'
   // baseUrl = 'http://ceshi.5ug.com'
   // baseUrl = 'http://localhost:9011'
-  // baseUrl = 'http://zkdebt.5ug.com'
+  //  baseUrl = 'http://zkdebt.5ug.com'
   // baseUrl = 'http://lll.5ug.com'
 } else {
   // baseUrl = 'http://zkdebt.5ug.com'
@@ -33,4 +33,10 @@ if (process.env.NODE_ENV === 'development') {
   // baseUrl = 'http://zkqd.5ug.com/'
   baseUrl = 'http://zqingchun.yiqipingou.com/'
 }
-export { baseUrl, routerMode, key, projectId, clientType }
+export {
+  baseUrl,
+  routerMode,
+  key,
+  projectId,
+  clientType
+}
