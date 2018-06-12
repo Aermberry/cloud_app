d1dd97f6538c2f0f0b9f1c8f2b648d9b4c6d9426
Salmonberry <sos2012happy@gmail.com>
2018-06-12 17:59:35 +0800

    shop_style

--- src/pages/shop/product/show.vue
+++ src/pages/shop/product/show.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="zkui-pc-productshow auto">
-    <header>
+    <header class="productshow-top-box">
+      世界，你好
       <div class="wrap">
-        <a href="">匠芯首页</a>
+        <a href="#">匠芯首页</a>
         <ul>
           <li>
             <a href="#">
@@ -41,48 +42,10 @@
 
 </script>
 <style lang="less" scoped>
-  // reset css
-  *,
-  ::before,
-  ::after {
-    padding: 0;
-    margin: 0;
-  }
-
-  zkui-pc-productshow {
-    font-size: 0.2rem;
-    color: #333;
-    /*设备默认的字体*/
-    font-family: 'Microsoft YaHei', sans-serif;
-  }
-
-  ul,
-  ol {
-    list-style: none;
-  }
-
-  a {
-    text-decoration: none;
-    color: #333;
-  }
-
-  a:hover {
-    color: #333;
-  }
-
-  input,
-  textarea {
-    border: none;
-    outline: none; /*border的外轮廓线，起突出作用*/
-    resize: none; /*用户手动调整大小*/
-    /*特殊的属性定义 清除浏览器给input自带的样式*/
-    -webkit-appearance: none; /*组件默认的样式（外观）为空*/
-  }
-
   // Public style
   .auto {
     margin: 0 auto;
-    width: 1200px;
+    width: 1200;
   }
   .fl {
     float: left;
@@ -95,15 +58,22 @@
     display: table;
     clear: both;
   }
-
+  a {
+    font-size: 10px;
+  }
   // Private style
 
   .zkui-pc-productshow {
     header {
-      height: 10px;
+      height: 50px;
       width: 100%;
       background-color: red;
-
+      font-size: 8px;
+      .wrap{
+        a {
+          // font-size: 10px;
+        }
+      }
     }
   }
 </style>
