7b884b8f932ba36ad6629ae3ebd017bed8a24f52
Salmonberry <sos2012happy@gmail.com>
2018-06-15 18:13:28 +0800

    nav

--- src/pages/shop/product/show.vue
+++ src/pages/shop/product/show.vue
@@ -14,7 +14,8 @@
           </li>
           <li>
             <a href="#">
-              我的订单
+              <span class="el-icon-tickets"></span>
+              <span>我的订单</span>
             </a>
           </li>
           <li>
@@ -25,6 +26,21 @@
           <li>
             <a href="#">
               客户服务
+            </a>
+          </li>
+           <li>
+            <a href="#">
+              <span class="el-icon-loading"></span>
+              <el-dropdown>
+                <span el-dropdown-link>商家后台</span>
+                <el-dropdown-menu slot="dropdown">
+    <el-dropdown-item>管理后台</el-dropdown-item>
+    <el-dropdown-item>商家社区</el-dropdown-item>
+    <el-dropdown-item>商家培训</el-dropdown-item>
+    <el-dropdown-item divided>市场入驻</el-dropdown-item>
+  </el-dropdown-menu>
+              </el-dropdown>
+
             </a>
           </li>
         </ul>
@@ -43,6 +59,7 @@
 <script>
 
 
+
 </script>
 
 
@@ -55,18 +72,25 @@
   .f_right {
     float: right;
   }
-
-  .clearfix::after,
-  .clearfix::before {
-    content: '.';
-    line-height: 0;
-    height: 0;
-    display: block;
-    visibility: hidden;
-    clear: both;
+  .m_r10 {
+    margin-left: 10px;
   }
 
+  .m_r10 {
+    margin-right: 10px;
+  }
+
+  .m_r10 {
+    margin-top: 10px;
+  }
+
+  .m_r10 {
+    margin-bottom: 10px;
+  }
+
+
   // Private style
+
   .zkui-pc-productshow {
     header {
       background-color: #f2f2f2;
@@ -74,46 +98,58 @@
         background-color: yellowgreen;
         margin: 0 auto;
         position: relative;
-        height: 31px;
+        height: 30px;
         max-width: 90%;
         min-width: 60%;
-
-        // background-color: black;
-        .el-icon-goods {
-        }
-
         .l-logo {
-          background-color: pink;
           text-align: center;
           line-height: 30px;
           height: 30px;
           width: 100px;
-          display: flex;
-          flex-wrap: nowrap;
-          justify-content: center;
-
+          display: flex; //启用flex布局
+          flex-flow: row nowrap; //flex是flex-direction和flex-wrap的缩写
+          justify-content: center; //主轴的对齐方式
+          align-items: center; //侧轴的对齐方式
           span {
-            line-height: 30px;
             font-size: 12px;
-            // display: block;
+          }
+          span:first-child {
+            line-height: 30px;
+            flex-basis: 26px; //调整第一个span在flex布局中的占空间的比例
           }
         }
         ul {
           position: absolute;
-          display: block;
+          // display: block;
           right: 10px;
           top: 0;
-
+          display: flex;
+          flex-flow: row nowrap;
+          justify-content: space-around;
+          width: 300px;
           // background-color: green;
-          // position: absolute;
-          right: 0;
-          li {
-            float: left;
-            a {
+          li{
+            // padding: 0 10px;
+            // border-left: 1px solid #eeeeee;
+            border: 0 1px solid #eeeeee;
+            height: 30px;
+            text-align: center;
+            a{
+              line-height: 30px;
+              height: 30px;
               display: block;
-              font-size: 18px;
+              // background-color: aquamarine;
+              font-size: 12px;
+              span {
+                font-size: 12px!important;
+              }
             }
           }
+
+        .dropdown {
+          font-size: 12px;
+          line-height: 30px;
+        }
         }
       }
     }
