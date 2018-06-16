87b67ee2fb998cf69de8baf34d672949b39d0e68
Salmonberry <sos2012happy@gmail.com>
2018-06-16 18:13:41 +0800

    nav_finish

--- src/pages/shop/product/show.vue
+++ src/pages/shop/product/show.vue
@@ -1,50 +1,55 @@
 <template>
   <div class="zkui-pc-productshow">
     <header class="productshow-top-box">
-      <div class="contain">
-        <a href="#" class="l-logo ">
-          <span class="el-icon-goods"></span>
-          <span>匠芯首页</span>
-        </a>
-        <ul>
-          <li>
-            <a href="#">
-              登陆
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <span class="el-icon-tickets"></span>
-              <span>我的订单</span>
-            </a>
-          </li>
-          <li>
-            <a href="#" class="el-icon-edit">
-              购物车
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              客户服务
-            </a>
-          </li>
-           <li>
-            <a href="#">
-              <span class="el-icon-loading"></span>
-              <el-dropdown>
-                <span el-dropdown-link>商家后台</span>
-                <el-dropdown-menu slot="dropdown">
-    <el-dropdown-item>管理后台</el-dropdown-item>
-    <el-dropdown-item>商家社区</el-dropdown-item>
-    <el-dropdown-item>商家培训</el-dropdown-item>
-    <el-dropdown-item divided>市场入驻</el-dropdown-item>
-  </el-dropdown-menu>
-              </el-dropdown>
+      <div class="nav">
+        <div class="contain">
+          <a href="#" class="l-logo ">
+            <span class="el-icon-goods"></span>
+            <span>匠芯首页</span>
+          </a>
+          <ul>
+            <li>
+              <a href="#">
+                登陆
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span class="el-icon-tickets"></span>
+                <span>我的订单</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" class="el-icon-edit">
+                购物车
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                客户服务
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span class="el-icon-loading"></span>
+                <el-dropdown :hide-on-click="false">
+                  <span class="el-dropdown-link">商家后台
+                    <i class="el-icon-arrow-down el-icon--right"></i>
+                  </span>
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item class="el-dropdown-item">管理后台</el-dropdown-item>
+                    <el-dropdown-item class="el-dropdown-item">商家社区</el-dropdown-item>
+                    <el-dropdown-item class="el-dropdown-item">商家培训</el-dropdown-item>
+                    <el-dropdown-item divided class="el-dropdown-item">市场入驻</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
 
-            </a>
-          </li>
-        </ul>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
+
     </header>
 
     <body>
@@ -61,6 +66,15 @@
 
 
 </script>
+/*  element组件无法修改时，另写一个样式 */
+<style lang="less">
+  .el-dropdown-menu {
+    .el-dropdown-item {
+      color: lighten(@metal, 10%);
+      font-size: 12px;
+    }
+  }
+</style>
 
 
 <style lang="less" scoped>
@@ -88,14 +102,19 @@
     margin-bottom: 10px;
   }
 
-
   // Private style
 
   .zkui-pc-productshow {
+    .nav {
+      height: auto;
+      width: 100%;
+      border-bottom: 1px solid lighten(@gray-600, 40%);
+      box-shadow: 0 0 5px 5px lighten(@gray-600, 40%);
+    }
     header {
       background-color: #f2f2f2;
       .contain {
-        background-color: yellowgreen;
+        // background-color: yellowgreen;
         margin: 0 auto;
         position: relative;
         height: 30px;
@@ -126,32 +145,42 @@
           display: flex;
           flex-flow: row nowrap;
           justify-content: space-around;
-          width: 300px;
-          // background-color: green;
-          li{
+          width: auto;
+          // background-color: pink;
+          li {
             // padding: 0 10px;
-            // border-left: 1px solid #eeeeee;
-            border: 0 1px solid #eeeeee;
-            height: 30px;
-            text-align: center;
-            a{
-              line-height: 30px;
-              height: 30px;
+
+            //  background-color: green;
+
+            height: 100%;
+            a {
+              border-right: 1px solid #eeeeee;
+              padding: 0 20px;
+              line-height: 9px;
+              height: auto;
               display: block;
-              // background-color: aquamarine;
               font-size: 12px;
+              margin-top: 10px;
               span {
-                font-size: 12px!important;
+                font-size: 12px !important;
+              }
+
+              .el-dropdown-link {
+                cursor: pointer;
+                color: #586c94;
+                .el-icon-arrow-down {
+                  font-size: 12px;
+                }
               }
             }
+            a:last-child {
+              border: none;
+            }
           }
-
-        .dropdown {
-          font-size: 12px;
-          line-height: 30px;
-        }
         }
       }
     }
   }
 </style>
+
+
