be248fd3d5c096a2b58550fcca085aec62f437be
sampsong <sampsong@qq.com>
2018-06-27 07:26:29 +0800

    修改文件函数

--- src/untils/file.js
+++ src/untils/file.js
@@ -1,26 +1,71 @@
-'use strict'
-
 const fs = require('fs')
-export default {
-  // 写入文件
-  write (path, data) {
-    fs.writeFile(path, data, function (err) {
-      if (err) {
-        console.log(err)
-      } else {
-        console.log('ok')
-      }
+const path = require('path')
+
+/**
+ * 文件监听函数
+ * @param {(String|String[])} files
+ * @param {Function} callback
+ */
+function watchFile(files, callback) {
+  [].concat(files).forEach((file) => {
+    let timer
+    fs.watch(file, () => {
+      if (timer) clearTimeout(timer)
+      timer = setTimeout(() => callback(file), 50)
     })
-  },
-  // 读取文件
-  read (path) {
-    fs.readFile(path, function (err, data) {
-      if (err) {
-        console.log(err)
-      } else {
-        console.log(data)
-        console.log(data.length + 'bayes')
-      }
+  })
+}
+
+/**
+ * 文件写入函数
+ * @param {String} file
+ * @param {*} data
+ */
+function writeFile(file, data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject(err)
+      resolve()
     })
+  })
+}
+
+/**
+ * 文件移除函数
+ * @param {(String|String[])} files
+ */
+function removeFile(files) {
+  return [].concat(files).map(file =>
+    new Promise((resolve, reject) => {
+      fs.unlink(file, (err) => {
+        if (err && err.code !== 'ENOENT') reject(err)
+        resolve()
+      })
+    }))
+}
+
+/**
+ * 依赖获取函数
+ * @param {String} file
+ */
+function resolveFile(file) {
+  const files = [file]
+  const fileDir = path.dirname(require.resolve(file))
+  const fileData = fs.readFileSync(file).toString()
+  const regexp = /require\(['|"](.*)['|"]\)/g
+
+  while (regexp.exec(fileData) !== null) {
+    const modulePath = path.join(fileDir, RegExp.$1)
+    files.push(require.resolve(modulePath))
   }
+
+  return files
+}
+
+
+module.exports = {
+  watchFile,
+  writeFile,
+  removeFile,
+  resolveFile
 }
